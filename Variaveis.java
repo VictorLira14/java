187a5d79a454a6c8e9ddc52fa36b8bc321768b1c
VictorLira14 <159541935+VictorLira14@users.noreply.github.com>
2025-08-07 09:51:58 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis{
+    public static void main(String[] args){
+
+        //tipo nome = conteúdo;
+
+        String meuNome = "Victor";
+        meuNome += " Lira";
+        int  idade = 18;
+        double altura = 1.78;
+        char sexo = 'M';
+        boolean  casado = true;
+        
+        System.out.println("Meu nome é"+meuNome+" Minha idade é "+idade+"anos, Minha Altura é"+altura);
+    }
+}
